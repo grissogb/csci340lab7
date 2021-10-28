ac525fe325fa6c4673e88c2268ceee4cc9f6b148
Garrett <grissogb@hendrix.edu>
2021-10-28 16:40:18 -0500

    RazorPages is in, I hope

---
+++ RazorPagesTrees/Models/Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesTrees.Models
+{
+    public class Tree
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PlantingDate { get; set; }
+        public decimal Age { get; set; }
+        public string Species { get; set; }
+        public decimal Height { get; set; }
+        public decimal Circumference { get; set; }
+
+
+
+    }
+}
