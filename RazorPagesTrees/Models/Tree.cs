982617c078d5927f5c1fdc281e31ba46481a9c0e
Garrett <grissogb@hendrix.edu>
2021-10-28 17:02:39 -0500

    >Now @ Part 6

--- RazorPagesTrees/Models/Tree.cs
+++ RazorPagesTrees/Models/Tree.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace RazorPagesTrees.Models
         public int ID { get; set; }
         public string Name { get; set; }
 
+        [Display(Name = "Planting Date")]
         [DataType(DataType.Date)]
         public DateTime PlantingDate { get; set; }
         public decimal Age { get; set; }
