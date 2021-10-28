9e98726830bab3dff473ab9a58bc645da5faaeaf
Garrett <grissogb@hendrix.edu>
2021-10-28 16:48:22 -0500

    Created the Initial Database

---
+++ RazorPagesTrees/Migrations/20211028214402_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesTrees.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tree",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlantingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Age = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Height = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Circumference = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tree", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tree");
+        }
+    }
+}
