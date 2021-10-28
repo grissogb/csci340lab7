a0bd8d82e89720735a810b323b96c7864e911be4
Garrett <grissogb@hendrix.edu>
2021-10-28 17:38:49 -0500

    Added Searching

---
+++ RazorPagesTrees/Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesTrees.Data;
+
+namespace RazorPagesTrees.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesTreesContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesTreesContext>>()))
+            {
+                // Look for any trees.
+                if (context.Tree.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Tree.AddRange(
+                    new Tree
+                    {
+                        Name = "Johnny",
+                        PlantingDate = DateTime.Parse("1989-2-12"),
+                        Age = 32,
+                        Species = "Birch",
+                        Height = 7.99M,
+                        Circumference = 2M
+                    },
+
+                    new Tree
+                    {
+                        Name = "Orange",
+                        PlantingDate = DateTime.Parse("1990-4-02"),
+                        Age = 31,
+                        Species = "Oak",
+                        Height = 8.34M,
+                        Circumference = 2.2M
+                    },
+
+                    new Tree
+                    {
+                        Name = "50 thousand",
+                        PlantingDate = DateTime.Parse("2002-12-19"),
+                        Age = 20,
+                        Species = "Pine",
+                        Height = 12.13M,
+                        Circumference = 3.25M
+                    },
+
+                    new Tree
+                    {
+                        Name = "Chocolate",
+                        PlantingDate = DateTime.Parse("2012-6-07"),
+                        Age = 09,
+                        Species = "Pecan",
+                        Height = 4M,
+                        Circumference = 1.36M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- RazorPagesTrees/Pages/Trees/Index.cshtml.cs
+++ RazorPagesTrees/Pages/Trees/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesTrees.Data;
 using RazorPagesTrees.Models;
@@ -21,9 +22,32 @@ namespace RazorPagesTrees.Pages.Trees
 
         public IList<Tree> Tree { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Species { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string TreeSpecies { get; set; }
+
         public async Task OnGetAsync()
         {
-            Tree = await _context.Tree.ToListAsync();
+            IQueryable<string> speciesQuery = from m in _context.Tree
+                                              orderby m.Species
+                                              select m.Species;
+            
+            var trees = from m in _context.Tree
+                        select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                trees = trees.Where(s => s.Name.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(TreeSpecies))
+            {
+                trees = trees.Where(x => x.Species == TreeSpecies);
+            }
+
+            Species = new SelectList(await speciesQuery.Distinct().ToListAsync());
+            Tree = await trees.ToListAsync();
         }
     }
 }
